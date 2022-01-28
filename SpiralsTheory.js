a2c436a22c32960a385b41a67268dfa294000a68
DanielGHebert <daniel.glen.hebert@gmail.com>
2022-01-28 11:32:06 -0800

    Progress on equations and costs

--- SpiralsTheory.js
+++ SpiralsTheory.js
@@ -56,8 +56,28 @@ var init = () => {
 
     // r1
 	{
-		let getDesc = (level) => "r_1=" + getr1(level).toString(2);
-		r1 = theory.createUpgrade(1, currency, new ExponentialCost(1,BigNumber.from("1.65282744595332527e-6")));
+		var costExp1 = BigNumber.from("1e100").log2() / BigNumber.from(88);
+		var costExp2 = BigNumber.from("1e100").log2() / BigNumber.from(1380);
+		var costExp3 = BigNumber.from("1e100").log2() / BigNumber.from(14699);
+		var costExp4 = BigNumber.from("1e100").log2() / BigNumber.from(133976);
+		var costExp5 = BigNumber.from("1e100").log2() / BigNumber.from(1380);
+		var costExp6 = BigNumber.from("1e100").log2() / BigNumber.from(1380);
+		var costExp7 = BigNumber.from("1e100").log2() / BigNumber.from(1380);
+		var costExp8 = BigNumber.from("1e100").log2() / BigNumber.from(1380);
+		var costExp9 = BigNumber.from("1e100").log2() / BigNumber.from(1380);
+		var costExp10 = BigNumber.from("1e100").log2() / BigNumber.from(1380);
+		let getDesc = (level) => "r_1=" + getr1(level).toString(0);
+		//r1 = theory.createUpgrade(1, currency, new ExponentialCost(1,BigNumber.from("1.65282744595332527e-6"))); //exponential cost to reach 1e1000
+		r1 = theory.createUpgrade(1, currency, new CompositeCost(89,new ExponentialCost(1, costExp1),
+											   new CompositeCost(1381,new ExponentialCost(BigNumber.from("1e100") * BigNumber.TWO.pow(costExp2), costExp2),
+											   new CompositeCost(14701,new ExponentialCost(BigNumber.from("1e200") * BigNumber.TWO.pow(costExp3), costExp3),
+											   new CompositeCost(133979,new ExponentialCost(BigNumber.from("1e300") * BigNumber.TWO.pow(costExp4), costExp4),
+											   new CompositeCost(89,new ExponentialCost(BigNumber.from("1e400") * BigNumber.TWO.pow(costExp5), costExp5),
+											   new CompositeCost(89,new ExponentialCost(BigNumber.from("1e500") * BigNumber.TWO.pow(costExp6), costExp6),
+											   new CompositeCost(89,new ExponentialCost(BigNumber.from("1e600") * BigNumber.TWO.pow(costExp7), costExp7),
+											   new CompositeCost(89,new ExponentialCost(BigNumber.from("1e700") * BigNumber.TWO.pow(costExp8), costExp8),
+											   new CompositeCost(89,new ExponentialCost(BigNumber.from("1e800") * BigNumber.TWO.pow(costExp9), costExp9),
+											   new ExponentialCost(BigNumber.from("1e900") * BigNumber.TWO.pow(costExp10), costExp10)))))))))));
 		r1.getDescription = (_) => Utils.getMath(getDesc(r1.level));
 		r1.getInfo = (amount) => Utils.getMathTo(getDesc(r1.level), getDesc(r1.level + amount));
 		//r1.boughtOrRefunded = (_) => theory.clearGraph();
@@ -66,8 +86,9 @@ var init = () => {
 	
 	// R
 	{
-		let getDesc = (level) => "R=" + getR(level).toString(2);
-		R = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(10,3.321928094887)));
+		let getDesc = (level) => "R=" + getR(level).toString(0);
+		//R = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(10,3.321928094887)));
+		R = theory.createUpgrade(0, currency, new FirstFreeCost(new CustomCost((level) => BigNumber.from(1.01) * r1.cost.getCost(parseInt(getR(level + 1).toString(0,0,Rounding.NEAREST)) - 2))));
 		R.getDescription = (_) => Utils.getMath(getDesc(R.level));
 		R.getInfo = (amount) => Utils.getMathTo(getDesc(R.level), getDesc(R.level + amount));
 		R.maxLevel = 100;
@@ -99,11 +120,13 @@ var tick = (elapsedTime, multiplier) => {
 	if (Rv != BigNumber.ZERO)
 	{
 		scalar = (BigNumber.ONE / (BigNumber.TWO * Rv));
-		var unscaledY = ((Rv - r1v) * (t * r1v / Rv).cos() + r2v * (t * (BigNumber.ONE - r1v/Rv)).cos());
-		var unscaledZ = ((Rv - r1v) * (t * r1v / Rv).sin() - r2v * (t * (BigNumber.ONE - r1v/Rv)).sin());
+		//var unscaledY = ((Rv - r1v) * (t * r1v / Rv).cos() + r2v * (t * (BigNumber.ONE - r1v/Rv)).cos());
+		//var unscaledZ = ((Rv - r1v) * (t * r1v / Rv).sin() - r2v * (t * (BigNumber.ONE - r1v/Rv)).sin());
+		var unscaledY = ((Rv - r1v) * (t).cos() + r2v * (t * (Rv - r1v)/r1v).cos());
+		var unscaledZ = ((Rv - r1v) * (t).sin() - r2v * (t * (Rv - r1v)/r1v).sin());
 		state.y = (scalar * unscaledY).toNumber();
 		state.z = (scalar * unscaledZ).toNumber();
-		rhodot = r1v * (unscaledY * unscaledY + unscaledZ * unscaledZ).sqrt() / BigNumber.from(GCD(parseInt(Rv.toString(0,0,Rounding.NEAREST)), parseInt(r1v.toString(0,0,Rounding.NEAREST))));
+		rhodot = r1v * r2v * Rv * (unscaledY * unscaledY + unscaledZ * unscaledZ).sqrt() / BigNumber.from(GCD(parseInt(Rv.toString(0,0,Rounding.NEAREST)), parseInt(r1v.toString(0,0,Rounding.NEAREST))));
 		currency.value += dt * rhodot;
 	}
 	
@@ -112,24 +135,27 @@ var tick = (elapsedTime, multiplier) => {
 
 var getPrimaryEquation = () => {
 	theory.primaryEquationHeight = 75;
-    let result = "\\dot{\\rho}=\\frac{r_1\\sqrt{x^2+y^2}}{GCD(r_1, R)}";
+	theory.primaryEquationScale = .9;
+    let result = "\\dot{\\rho}=\\frac{r_1 r_2 R\\sqrt{x^2+y^2}}{gcd(r_1, R)}" +
+	"\\qquad \\begin{matrix} x=L\\cos(\\theta) + r_2\\cos(\\theta L r_1^{-1} )\\\\" +
+	"y=L\\sin(\\theta) - r_2\\sin(\\theta L r_1^{-1}) \\end{matrix}";
     return result;
 }
 
 var getSecondaryEquation = () => {
-	theory.secondaryEquationHeight = 100;
-	return "x=(R-r_1)\\cos(t\\frac{r_1}{R}) + r_2\\cos(t(1-\\frac{r_1}{R}))\\\\" + 
-	       "y=(R-r_1)\\sin(t\\frac{r_1}{R}) + r_2\\sin(t(1-\\frac{r_1}{R}))\\\\" + 
-		   "r_2=r_1(\\sin(\\pi\\frac{t}{500})+1)";
+	theory.secondaryEquationHeight = 300;
+	theory.secondaryEquationScale = 1;
+	return "";//\\\\" + 
+		   //""; //(\\sin(\\pi\\frac{t}{500})+1)";
 }
 
 var getTertiaryEquation = () => {
-	return "t=" + t.toString(2) +
+	return "L=(R-r_1),r_2=.5r_1, \\theta =" + t.toString(2) +
                         ",r_2=" + getr2().toString(2) +
 						//",x=" + BigNumber.from(state.y).toString(2) +
 						//",y=" + BigNumber.from(state.z).toString(2) + 
 						",\\dot{\\rho}=" + rhodot.toString(2) + 
-						",GCD(R, r1)=" + GCD(parseInt(getR(R.level).toString(0,0,Rounding.NEAREST)), parseInt(getr1(r1.level).toString(0,0,Rounding.NEAREST)));
+						",GCD(R, r1)=" + GCD(parseInt(getR(R.level).toString(0,0,Rounding.NEAREST)), parseInt(getr1(r1.level).toString(0,0,Rounding.NEAREST)));	
 }
 
 var getPublicationMultiplier = (tau) => tau.pow(0.164) / BigNumber.THREE;
@@ -163,7 +189,7 @@ var getR = (level) => {
 	return BigNumber.from(result);
 }
 var getr1 = (level) => BigNumber.from(level + 1);
-var getr2 = () => getr1(r1.level) * ((BigNumber.PI * t / BigNumber.from(500)).sin() + BigNumber.ONE);
+var getr2 = () => getr1(r1.level) * .5;//getr1(r1.level) * ((BigNumber.PI * t / BigNumber.from(500)).sin() + BigNumber.ONE);
 var getC1Exponent = (level) => BigNumber.from(1 + 0.05 * level);
 var getC2Exponent = (level) => BigNumber.from(1 + 0.05 * level);
 
